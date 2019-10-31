1c6036435941bbf4a2bed3fb7edd158029e1810f
wanchenghuang <1782331304@qq.com>
2019-10-31 13:04:33 +0800

    add file(国内地区）

--- tickets-parent/tickets-data/src/main/java/com/track/data/mapper/ticket/AreaRegionMapper.java
+++ tickets-parent/tickets-data/src/main/java/com/track/data/mapper/ticket/AreaRegionMapper.java
@@ -2,6 +2,10 @@ package com.track.data.mapper.ticket;
 
 import com.track.data.domain.po.ticket.AreaRegionPo;
 import com.track.data.mapper.base.IBaseMapper;
+import com.track.data.vo.manage.ticket.AreaCityVo;
+import com.track.data.vo.manage.ticket.AreaVo;
+
+import java.util.List;
 
 /**
  * <p>
@@ -13,4 +17,39 @@ import com.track.data.mapper.base.IBaseMapper;
  */
 public interface AreaRegionMapper extends IBaseMapper<AreaRegionPo> {
 
+    /**
+     * @Author chauncy
+     * @Date 2019-10-31 11:17
+     * @Description //获取省市区
+     *
+     * @Update chauncy
+     *
+     * @param
+     * @return java.util.List<com.track.data.vo.manage.ticket.AreaVo>
+     **/
+    List<AreaVo> searchList();
+
+    /**
+     * @Author chauncy
+     * @Date 2019-10-31 11:18
+     * @Description //获取省市
+     *
+     * @Update chauncy
+     *
+     * @param
+     * @return java.util.List<com.track.data.vo.manage.ticket.AreaCityVo>
+     **/
+    List<AreaCityVo> search();
+
+    /**
+     * @Author chauncy
+     * @Date 2019-10-31 11:19
+     * @Description //根据区县编号获取街道信息
+     *
+     * @Update chauncy
+     *
+     * @param  parentCode
+     * @return java.util.List<com.track.data.vo.manage.ticket.AreaVo>
+     **/
+    List<AreaVo> findStreet(String parentCode);
 }
